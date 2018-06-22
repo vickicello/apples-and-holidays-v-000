c5697c59936cea802d1e1eaed8c444c11722c096
Vicki Hamm <vicki.cello@gmail.com>
2018-06-22 19:40:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ def all_supplies_in_holidays(holiday_hash)
             holiday.each do |attribute, supplies|
               puts "#{attribute.to_s.split("_").map(&:capitalize).join(" ")}: #{supplies.join(", ")}"
             end
-          end
+        end
 end
 
 def all_holidays_with_bbq(holiday_hash)
